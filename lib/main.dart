77897afb0a730a8f8309c243c40fd25f3e173b20
Rico Kunz <rico.kunz@leutek.de>
2023-02-09 13:35:10 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_msd/screens/login_screen.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'MSD',
+      home: Scaffold(
+        // appBar: AppBar(
+        //   title: const Text('MSD'),
+        // ),
+        body: SafeArea(
+          child: LoginScreen(),
+        ),
+      ),
+    );
+  }
+}
